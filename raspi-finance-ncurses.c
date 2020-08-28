7c2547ba54b04eb51409ab9aaf8fce3cd20f1b4e
Brian Henning <henninb@msn.com>
2020-08-28 17:05:26 -0500

    changed out the cleared to transactionState

--- raspi-finance-ncurses.c
+++ raspi-finance-ncurses.c
@@ -17,7 +17,7 @@
 #define AMOUNT "amount"
 #define DESCRIPTION "description"
 #define CATEGORY "category"
-#define CLEARED "cleared"
+#define TRANSACTION_STATE "transactionState"
 #define NOTES "notes"
 #define GUID "guid"
 #define REOCCURRING "reoccurring"
@@ -46,7 +46,7 @@ typedef enum {
     TRANSACTION_DESCRIPTION,
     TRANSACTION_CATEGORY,
     TRANSACTION_AMOUNT,
-    TRANSACTION_CLEARED,
+    TRANSACTION_TRANSACTION_STATE,
     TRANSACTION_NOTES,
 
     MAX_TRANSACTION
@@ -305,8 +305,8 @@ void set_transaction_default_values() {
     set_field_buffer(fields[TRANSACTION_CATEGORY * 2 + 1], 0, "");
     set_field_buffer(fields[TRANSACTION_AMOUNT * 2], 0, AMOUNT);
     set_field_buffer(fields[TRANSACTION_AMOUNT * 2 + 1], 0, "");
-    set_field_buffer(fields[TRANSACTION_CLEARED * 2], 0, CLEARED);
-    set_field_buffer(fields[TRANSACTION_CLEARED * 2 + 1], 0, "0");
+    set_field_buffer(fields[TRANSACTION_TRANSACTION_STATE * 2], 0, TRANSACTION_STATE);
+    set_field_buffer(fields[TRANSACTION_TRANSACTION_STATE * 2 + 1], 0, "outstanding");
     set_field_buffer(fields[TRANSACTION_NOTES * 2], 0, NOTES);
     set_field_buffer(fields[TRANSACTION_NOTES * 2 + 1], 0, "processed by ncurses");
     set_field_buffer(fields[TRANSACTION_ACCOUNT_NAME_OWNER * 2], 0, ACCOUNT_NAME_OWNER);
@@ -358,7 +358,7 @@ int transaction_json_generated() {
     snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":\"%s\",", DESCRIPTION, extract_field(fields[TRANSACTION_DESCRIPTION * 2 + 1]));
     snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":\"%s\",", CATEGORY, extract_field(fields[TRANSACTION_CATEGORY * 2 + 1]));
     snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":%s,", AMOUNT, extract_field(fields[TRANSACTION_AMOUNT * 2 + 1]));
-    snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":%s,", CLEARED, extract_field(fields[TRANSACTION_CLEARED * 2 + 1]));
+    snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":\"%s\",", TRANSACTION_STATE, extract_field(fields[TRANSACTION_TRANSACTION_STATE * 2 + 1]));
     snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":\"%s\",", NOTES, extract_field(fields[TRANSACTION_NOTES * 2 + 1]));
     snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":\"%s\",", ACCOUNT_TYPE, extract_field(fields[TRANSACTION_ACCOUNT_TYPE * 2 + 1]));
     snprintf(payload + strlen(payload), sizeof(payload), "\"%s\":\"%s\",", ACCOUNT_NAME_OWNER, extract_field(fields[TRANSACTION_ACCOUNT_NAME_OWNER * 2 + 1]));
